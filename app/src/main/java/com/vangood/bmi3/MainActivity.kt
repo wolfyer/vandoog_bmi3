f31badcafda83dd1f80f38ef62cd1e2ad815ce4c
Class <a90023y@gmail.com>
2022-03-04 10:49:48 +0800

    clear bmi commit

--- app/src/main/java/com/vangood/bmi3/MainActivity.kt
+++ app/src/main/java/com/vangood/bmi3/MainActivity.kt
@@ -2,10 +2,33 @@ package com.vangood.bmi3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.vangood.bmi3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun calculatebmi(view: View){
+        val weight = binding.edWeight.text.toString().toFloat()
+        val height = binding.edHeight.text.toString().toFloat()
+        val bmi = weight/(height*height)
+        //Builder
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("your bmi is $bmi")
+        builder.show()
+
+    }
+    fun clearall(view: View){
+        binding.edWeight.setText("")
+        binding.edHeight.setText("")
+
     }
 }
